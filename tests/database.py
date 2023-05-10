e7aeb8d7d418b84de220a979d9514c8f31c80d0c
user <mihir.shah@sleevesup.com.au>
2023-05-10 23:09:25 +0530

    ci/cd with testcases

--- tests/database.py
+++ tests/database.py
@@ -14,7 +14,8 @@ from app.database.database import Base
 import psycopg2
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+# SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://rental_system:postgres@localhost:5432/rental_postgres_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
