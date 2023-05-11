6a6fb7d7d3c561c8b861b2e3e4d6e01372525c0f
user <mihir.shah@sleevesup.com.au>
2023-05-11 10:58:22 +0530

    ci with multiple db

--- app/config.py
+++ app/config.py
@@ -7,6 +7,7 @@ class Settings(BaseSettings):
     database_password: str
     database_name: str
     database_username: str
+    database_port_testing: str
     secret_key: str
     algorithm: str
     token_expiry: int
--- tests/database.py
+++ tests/database.py
@@ -14,7 +14,7 @@ from app.database.database import Base
 import psycopg2
 
 
-SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}_test'
+SQLALCHEMY_DATABASE_URL = f'postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port_testing}/{settings.database_name}_test'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
