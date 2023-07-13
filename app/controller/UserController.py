03c6a50210d0fedb76b57b72bf949e2a9379cfbc
Mihir <mihir.shah@sleevesup.com.au>
2023-07-13 16:12:29 +0530

    added a publish method

--- app/controller/UserController.py
+++ app/controller/UserController.py
@@ -6,6 +6,7 @@ from app.database.database import get_db
 from app.schema.UserSchema import UserOut
 from app.oauth2 import get_current_user
 from app import main as main
+import json
 
 
 router = APIRouter(
@@ -16,7 +17,10 @@ router = APIRouter(
 
 @router.post("/signup", status_code=status.HTTP_201_CREATED, response_model=UserOut)
 async def signup(user: Signup, db:  Session = Depends(get_db)):
-    return UserService.signup(user, db)
+    status = publish_to_rabbitmq(json.dumps(user))
+    if status == 400:
+
+        return UserService.signup(user, db)
 
 
 @router.get("", status_code=status.HTTP_200_OK, response_model=UserOut)
@@ -25,12 +29,15 @@ async def get_current_user(db: Session = Depends(get_db), email: str = Depends(g
     return user
 
 
-@router.post("/publish")
+# @router.post("/publish")
 async def publish_to_rabbitmq(message: str):
-    connection = main().get_rabbitmq_connection()
+    connection = main.get_rabbitmq_connection()
     channel = connection.channel()
     channel.queue_declare(queue='fast_api_queue')
-    channel.basic_publish(
-        exchange='', routing_key='fast_api_queue', body=message)
-    connection.close()
-    return {"message": "Message published successfully"}
+    try:
+        channel.basic_publish(
+            exchange='', routing_key='fast_api_queue', body=message)
+        connection.close()
+        return 200
+    except Exception as e:
+        print(e)
